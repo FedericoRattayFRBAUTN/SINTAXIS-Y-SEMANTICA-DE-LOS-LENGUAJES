f921632ce52d8e6c9b048f60c7f41323420a7612
Rattay <130920389+FedericoRattayFRBAUTN@users.noreply.github.com>
2023-04-23 19:42:31 -0300

    Update hello.c

--- 00-CHelloworld/hello.c
+++ 00-CHelloworld/hello.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-    FILE *file;
-    char text[] = "Hello, World!"; // Texto por guardar
-
-    file = fopen("output.txt", "w"); // Abrir archivo en modo de lectura
-    if (file == NULL)                // Verificar que el archivo abrio correctamente
-    {
-        printf("Error opening file.\n");
-        return 1;
-    }
-    else
-    {
-        fprintf(file, "%s", text); // Escribir el archivo
-        fclose(file);              // Cerrar el archivo
-    }
-
+    printf("Hello, World!\n");
+    
     return 0;
 }
